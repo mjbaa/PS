a275146feaaba9544110f2a9ab0e37e7e045a8f6
mjbaa <wnsqja7545@gmail.com>
2025-02-03 17:31:13 +0900

    [Gold V] Title: 토마토, Time: 580 ms, Memory: 127980 KB -BaekjoonHub

---
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -0,0 +1,78 @@
+import java.util.*;
+import java.io.*;
+
+
+public class Main {
+    public static int N;
+    public static int M;
+    public static int[][] box;
+
+    public static int[] dx = {1, -1, 0, 0};
+    public static int[] dy = {0, 0, 1, -1};
+
+    public static Queue<int[]> queue = new LinkedList<>();
+
+
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        M = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+
+        box = new int[N][M];
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            for (int j = 0; j < M; j++) {
+                box[i][j] = Integer.parseInt(st.nextToken());
+                if(box[i][j] == 1){
+                    queue.offer(new int[] {i,j,0});
+                }
+
+            }
+        }
+
+        bfs();
+    }
+
+    public static void bfs() {
+        int day = 0;
+
+        while(!queue.isEmpty()) {
+            int[] t = queue.poll();
+            day = t[2];
+
+            for(int i=0; i<4; i++) {
+                int nx = t[0] + dx[i];
+                int ny = t[1] + dy[i];
+
+                if(0 <= nx && nx <N && 0<= ny && ny <M) {
+                    if(box[nx][ny] == 0) {
+                        box[nx][ny] = 1;
+                        queue.add(new int[] {nx, ny, day+1});
+                    }
+                }
+            }
+        }
+
+        if(checkTomato()){
+            System.out.println(day);
+        } else{
+            System.out.println(-1);
+        }
+
+    }
+
+    static boolean checkTomato() {
+        for(int i=0; i<N; i++) {
+            for(int j=0; j<M; j++) {
+                if(box[i][j] == 0)
+                    return false;
+            }
+        }
+        return true;
+    }
+
+}
