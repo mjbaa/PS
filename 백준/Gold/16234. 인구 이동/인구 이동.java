ac01b543cdfd0da9238976d60fbfce09da148946
mjbaa <wnsqja7545@gmail.com>
2025-02-25 10:24:08 +0900

    [Gold IV] Title: 인구 이동, Time: 988 ms, Memory: 300724 KB -BaekjoonHub

--- 백준/Gold/16234. 인구 이동/인구 이동.java
+++ 백준/Gold/16234. 인구 이동/인구 이동.java
@@ -11,6 +11,7 @@ class Main {
 	static int[] dy = {1,-1,0,0};
 	
 	static List<List<int[]>> list = new ArrayList<>();
+	//list : 하루에 생성되는 연합들의 list
 	
 	static int days=0;
 	
@@ -20,15 +21,16 @@ class Main {
 			for(int j=0;j<n;j++) {
 				if(!visited[i][j]) {//방문x 국가 발견 -> 새로운 연합
 					
-					bfs(i,j);//연합에 국가 더하기
+					bfs(i,j);//list에 new 연합 추가, 해당 연합에 국가 더하기
 				}
 			}
 		}
 	}
 	
 	static void bfs(int i, int j) {
-		List<int[]> union = new ArrayList<>();
-		Queue<int[]> queue = new LinkedList<>();
+		List<int[]> union = new ArrayList<>();//추가할 연합 생성
+		
+		Queue<int[]> queue = new LinkedList<>();//for bfs
 		queue.add(new int[] {i,j});
 		visited[i][j] = true;
 		
@@ -39,19 +41,18 @@ class Main {
 			int x = cur[0];
 			int y = cur[1];
 			
-			
-			
 			for(int k=0;k<4;k++) {
 				int nx = x + dx[k];
 				int ny = y + dy[k];
 				
 				if(nx<0 || ny < 0|| n <= nx || n <= ny) continue;
 				if(visited[nx][ny]) continue;
-						
+				//visited[nx][ny] = true; 
+				//-> 여기에 true 할 경우 현재는 갈 수 없어도 다른 칸에서 갈 수 있는 경우 체크 못함
 				
 				int diff = Math.abs(data[x][y] - data[nx][ny]); 
 				if(L <= diff && diff <= R) {
-					visited[nx][ny] = true; // v 위치!!!
+					visited[nx][ny] = true; // v 위치!!! 
 					union.add(new int[] {nx,ny});
 					queue.add(new int[] {nx,ny});
 				}
@@ -62,10 +63,11 @@ class Main {
 	}
 	
 	static boolean move() {
-		// 이동이 있으면 true : 연합이어도 이동X일수도
+		// 이동이 있으면 true : 연합이어도 이동X 가능
+		// 연합 내에 하나라도 다르면 이동O
 		boolean flag = false;
 		
-		for(int i=0;i<list.size();i++) {// 연합 내에 하나라도 다르면 이동O
+		for(int i=0;i<list.size();i++) {
 			Set<Integer> set = new HashSet<>();
 			
 			List<int[]> union = list.get(i);
@@ -74,11 +76,12 @@ class Main {
 				sum += data[country[0]][country[1]];
 				set.add(data[country[0]][country[1]]);
 			}
-			
-			if(set.size() != 1) flag = true;; 
-			
+
 			//연합 내 모든 국가 인구수 동일 -> 이동 X
-			//한 연합의 한 국가라도 움직이면 이동 O
+			//한 연합의 한 국가라도 움직이면 해당 일 이동 O
+			//한 연합의 한 국가라도 인구 수가 다른 나라와 다른 경우 이동
+			if(set.size() != 1) flag = true;; 
+
 			
 			int val = sum / union.size();
 			
@@ -89,7 +92,7 @@ class Main {
 			
 		}
 
-		list = new ArrayList<>(); // 해당 일 이동 끝 -> list 초기화
+		list = new ArrayList<>(); // 해당 일 끝 -> list 초기화
 		return flag;
 	}
 	
@@ -113,45 +116,12 @@ class Main {
     		boolean flag = move();
     		if(flag) {//이동 O
     			days++;
-    		}else {
+    		}else {//이동 x
     			break;
     		}
     	}
     	System.out.println(days);
-    	
-    	
-//    	for(int i=0;i<n;i++) {
-//    		for(int j=0;j<n;j++) {
-//    			System.out.print(data[i][j] +" ");
-//    		}
-//    		System.out.println();
-//    	}
-//    	System.out.println();
-//    	
-//    	findUnion();
-//    	//	static List<List<int[]>> list = new ArrayList<>();
-//    	for(int i=0;i<list.size();i++) {
-//    		System.out.println("list : "+i);
-//    		for(int[] d : list.get(i)) {
-//    			System.out.print(" x : "+d[0]+" y : "+d[1]+ "|");
-//    		}
-//    		System.out.println();
-//    	}
-//    	boolean flag = move();
-//    	if(flag) days++;
-//    	
-//    	for(int i=0;i<n;i++) {
-//    		for(int j=0;j<n;j++) {
-//    			System.out.print(data[i][j] +" ");
-//    		}
-//    		System.out.println();
-//    	}
-//    	
-//    	System.out.println(days);
-    	
-    	
-    	
-    	
+    	  		
     }
     
 }
