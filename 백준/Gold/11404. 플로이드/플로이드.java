4975931cbeda8391b1d1370368735b456aaff842
mjbaa <wnsqja7545@gmail.com>
2025-04-04 09:37:43 +0900

    [Gold IV] Title: 플로이드, Time: 388 ms, Memory: 42908 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.java
+++ 백준/Gold/11404. 플로이드/플로이드.java
@@ -9,46 +9,48 @@ public class Main {
         StringTokenizer st;
         StringBuilder sb = new StringBuilder();
         
+        final int INF = 1000000000;
+        
         int n = Integer.parseInt(br.readLine());
         int m = Integer.parseInt(br.readLine());
         int[][] data = new int[n][n];
-        boolean[][] can = new boolean[n][n];
+        
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(data[i], INF);
+            data[i][i] = 0;
+        }
+        
         for(int i=0;i<m;i++) {
         	st = new StringTokenizer(br.readLine());
         	int s = Integer.parseInt(st.nextToken())-1;
         	int d = Integer.parseInt(st.nextToken())-1;
         	int w = Integer.parseInt(st.nextToken());
-        	can[s][d] = true;
         	
-        	if(data[s][d] != 0) data[s][d] = Math.min(data[s][d], w);
-        	else data[s][d] = w;
+        	data[s][d] = Math.min(data[s][d], w);
+
         }
         
-        for(int i=0;i<n;i++) {
-        	data[i][i] = 0;
-        	can[i][i] = true;
-        }
         
         for(int k=0;k<n;k++) {
         	for(int i=0;i<n;i++) {
         		for(int j=0;j<n;j++) {
-        			if(can[i][j]) {
-        				if(can[i][k] && can[k][j]) {
-        					data[i][j] = Math.min(data[i][j], data[i][k] + data[k][j]);
-        				}//else : data[i][j] 그대로
-        			}else {
-        				if(can[i][k] && can[k][j]) {
-        					data[i][j] = data[i][k] + data[k][j];
-        					can[i][j] = true;
-        				}
-        			}
+        			if(data[i][k] != INF && data[k][j] != INF) {
+        				data[i][j] = Math.min(data[i][j], data[i][k] + data[k][j]);
+        			}//else : data[i][j] 그대로
+        			
         		}
         	}
         }
         
         for(int i=0;i<n;i++) {
         	for(int j=0;j<n;j++) {
-        		sb.append(data[i][j]).append(" ");
+        		if (data[i][j] == INF) {
+        		    sb.append("0");
+        		} else {
+        		    sb.append(data[i][j]);
+        		}
+        		sb.append(" ");
+        		
         	}
         	sb.append("\n");
         }
