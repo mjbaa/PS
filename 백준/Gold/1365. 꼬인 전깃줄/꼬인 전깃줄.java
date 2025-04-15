df43fa0b4cb5a04cdc51fe25ea81513ef10f2ff5
mjbaa <wnsqja7545@gmail.com>
2025-04-15 12:11:42 +0900

    [Gold II] Title: 꼬인 전깃줄, Time: 324 ms, Memory: 28892 KB -BaekjoonHub

---
+++ 백준/Gold/1365. 꼬인 전깃줄/꼬인 전깃줄.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int[] data = new int[n+1];
+        for(int i=1;i<=n;i++) {
+        	data[i] = Integer.parseInt(st.nextToken());
+        }
+        
+        List<Integer> lis = new ArrayList<>();
+        
+        for(int i=1;i<=n;i++) {
+        	int idx = Collections.binarySearch(lis, data[i]);
+        	
+        	if(idx < 0) {
+        		idx ++;
+        		idx *= -1;
+        	}
+        	
+        	if(idx == lis.size()) {
+        		lis.add(data[i]);
+        	}else {
+        		lis.set(idx, data[i]);
+        	}
+        }
+        
+        System.out.println(n - lis.size());
+    }
+}
