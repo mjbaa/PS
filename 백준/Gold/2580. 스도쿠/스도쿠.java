4b970bea23a1f11b2fad7a48d3cc09d5127a80c5
mjbaa <wnsqja7545@gmail.com>
2025-05-10 20:01:29 +0900

    [Gold IV] Title: 스도쿠, Time: 252 ms, Memory: 17820 KB -BaekjoonHub

--- 백준/Gold/2580. 스도쿠/스도쿠.java
+++ 백준/Gold/2580. 스도쿠/스도쿠.java
@@ -3,77 +3,63 @@ import java.util.*;
 
 public class Main {
     static int[][] board = new int[9][9];
-    static boolean[][][] visited = new boolean[9][9][10];
+    static boolean[][] row = new boolean[9][10];       // row[i][num]
+    static boolean[][] col = new boolean[9][10];       // col[j][num]
+    static boolean[][] square = new boolean[9][10];    // square[k][num]
     static List<int[]> blanks = new ArrayList<>();
-    static int bSize = 0;
-
+    static boolean solved = false;
     static StringBuilder sb = new StringBuilder();
-    
-    static boolean finished = false;
-    static void dfs(int bidx){
-        if(finished) return;
-        
-        if(bidx >= bSize) {
-            for(int i = 0; i < 9; i++) {
-                for(int j = 0; j < 9; j++) {
+
+    static void dfs(int idx) {
+        if (solved) return;
+
+        if (idx == blanks.size()) {
+            for (int i = 0; i < 9; i++) {
+                for (int j = 0; j < 9; j++) {
                     sb.append(board[i][j]).append(" ");
                 }
                 sb.append("\n");
             }
             System.out.println(sb);
-            finished = true;
+            solved = true;
             return;
         }
 
-        int[] blank = blanks.get(bidx);
-        int bx = blank[0];
-        int by = blank[1];
+        int[] blank = blanks.get(idx);
+        int x = blank[0], y = blank[1];
+        int s = (x / 3) * 3 + (y / 3);
 
-        Arrays.fill(visited[bx][by], false);
+        for (int num = 1; num <= 9; num++) {
+            if (row[x][num] || col[y][num] || square[s][num]) continue;
 
-        //xy축
-        for(int i=0;i<9;i++){
-            visited[bx][by][board[i][by]] = true;
-            visited[bx][by][board[bx][i]] = true;
+            board[x][y] = num;
+            row[x][num] = col[y][num] = square[s][num] = true;
+
+            dfs(idx + 1);
+
+            board[x][y] = 0;
+            row[x][num] = col[y][num] = square[s][num] = false;
         }
-
-        //작은 정사각형
-        int nx = (bx / 3) * 3;
-        int ny = (by / 3) * 3;
-        for(int i=0;i<3;i++){
-            for(int j=0;j<3;j++){
-                visited[bx][by][board[nx+i][ny+j]] = true;
-            }
-        }
-
-        for (int i = 1; i <= 9; i++) {
-            if (visited[bx][by][i]) continue;
-
-            board[bx][by] = i;
-            dfs(bidx + 1);
-            board[bx][by] = 0;
-        }
-
-
-
-
     }
 
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
-        for(int i = 0; i < 9; i++) {
+        for (int i = 0; i < 9; i++) {
             st = new StringTokenizer(reader.readLine());
-            for(int j = 0; j < 9; j++) {
+            for (int j = 0; j < 9; j++) {
                 board[i][j] = Integer.parseInt(st.nextToken());
-                if(board[i][j] == 0) blanks.add(new int[]{i, j});
+                if (board[i][j] == 0) {
+                    blanks.add(new int[]{i, j});
+                } else {
+                    int num = board[i][j];
+                    row[i][num] = true;
+                    col[j][num] = true;
+                    square[(i / 3) * 3 + (j / 3)][num] = true;
+                }
             }
         }
 
-        bSize = blanks.size();
-
         dfs(0);
-
-
     }
 }
