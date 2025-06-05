2d6b70b073a6eadd46ad3b9340a71426849a224e
mjbaa <wnsqja7545@gmail.com>
2025-06-05 15:28:23 +0900

    [Gold III] Title: 웜홀, Time: 388 ms, Memory: 21920 KB -BaekjoonHub

--- 백준/Gold/1865. 웜홀/웜홀.java
+++ 백준/Gold/1865. 웜홀/웜홀.java
@@ -1,88 +1,83 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-	static class Edge{
-		int from, to, weight;
-		Edge(int from, int to, int weight){
-			this.from = from;
-			this.to = to;
-			this.weight = weight;
-		}
-	}
-	
-	
-	static final int INF = Integer.MAX_VALUE;
-	static int n,m,w;
-	static int[] distance;
-	static List<Edge> edges;
-	
-	static boolean bf() {
+    static final long INF = Long.MAX_VALUE;
 
-		Arrays.fill(distance, 0);
-		
-		for(int i=0;i<n-1;i++) {
-			for(Edge edge : edges) {
-				if(distance[edge.from] != INF && distance[edge.to] > distance[edge.from]  +edge.weight) {
-					distance[edge.to] = distance[edge.from]  +edge.weight;
-				}
-			}
-		}
-		
-		for(Edge edge : edges) {
-			if(distance[edge.from] != INF && distance[edge.to] > distance[edge.from]  +edge.weight) {
-				return false;
-			}
-		}
-		return true;
-	}
-	public static void main(String[] args) throws IOException {
+    static class Edge{
+        int from,to,weight;
+        Edge(int from,int to,int weight){
+            this.from = from;
+            this.to = to;
+            this.weight = weight;
+        }
+    }
+    static int n,m,w;
+    static long[] dist;
+    static List<Edge> edges;
+
+    static boolean bf(){
+        for(int i=0;i<n-1;i++){
+            for(Edge e : edges){
+                if(dist[e.from] != INF && dist[e.to] > dist[e.from] + e.weight){
+                    dist[e.to] = dist[e.from] + e.weight;
+                }
+            }
+        }
+
+        for(Edge e : edges){
+            if(dist[e.from] != INF && dist[e.to] > dist[e.from] + e.weight){
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         StringBuilder sb = new StringBuilder();
-        int t = Integer.parseInt(br.readLine());
-        for(int tc = 1;tc<=t;tc++) {
-        	st = new StringTokenizer(br.readLine());
-        	n = Integer.parseInt(st.nextToken());
-        	m = Integer.parseInt(st.nextToken());
-        	w = Integer.parseInt(st.nextToken());
-        	
-        	distance = new int[n+1];
-        	
-        	
-        	edges = new ArrayList<>();
-        	
-        	for(int i=0;i<m;i++) {
-        		st = new StringTokenizer(br.readLine());
-        		int s = Integer.parseInt(st.nextToken());
-        		int d = Integer.parseInt(st.nextToken());
-        		int w = Integer.parseInt(st.nextToken());
-        		edges.add(new Edge(s,d,w));
-        		edges.add(new Edge(d,s,w));
-        	}
-        	
-        	for(int i=0;i<w;i++) {
-        		st = new StringTokenizer(br.readLine());
-        		int s = Integer.parseInt(st.nextToken());
-        		int d = Integer.parseInt(st.nextToken());
-        		int w = Integer.parseInt(st.nextToken());
-        		edges.add(new Edge(s,d,-1 * w));
-        	}
-        	
 
-        	
-        	if(bf()) {
-        		sb.append("NO\n");
-        	}else {
-        		sb.append("YES\n");
-        	}
-        	
-        	
-        	
-        	
+        int t = Integer.parseInt(br.readLine());
+        for(int tc=0;tc<t;tc++){
+            edges = new ArrayList<>();
+            st = new StringTokenizer(br.readLine());
+            n = Integer.parseInt(st.nextToken());
+            m = Integer.parseInt(st.nextToken());
+            w = Integer.parseInt(st.nextToken());
+
+            dist = new long[n+1];
+            Arrays.fill(dist,0);
+//            dist[1] = 0;
+
+            for(int i=0;i<m;i++){
+                st = new StringTokenizer(br.readLine());
+                int start = Integer.parseInt(st.nextToken());
+                int from = Integer.parseInt(st.nextToken());
+                int weight = Integer.parseInt(st.nextToken());
+                edges.add(new Edge(start,from,weight));
+                edges.add(new Edge(from,start,weight));
+
+            }
+
+            for(int i=0;i<w;i++){
+                st = new StringTokenizer(br.readLine());
+                int start = Integer.parseInt(st.nextToken());
+                int from = Integer.parseInt(st.nextToken());
+                int weight = -1 * Integer.parseInt(st.nextToken());
+                edges.add(new Edge(start,from,weight));
+            }
+
+            if(bf()){
+                sb.append("NO\n");
+            }else{
+                sb.append("YES\n");
+            }
+
         }
-        System.out.print(sb);
-	}
-	
-	
+
+        System.out.println(sb);
+
+    }
 }
