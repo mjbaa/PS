0cf50b3d2b35a62309d2ac296c11283921a732f6
mjbaa <wnsqja7545@gmail.com>
2025-06-07 16:47:29 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 652 ms, Memory: 114580 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
@@ -1,85 +1,62 @@
 import java.util.*;
 import java.io.*;
 
-
 public class Main {
+    static int n,m;
     static int[][] data;
-    static int[][][] visited; //좌표, 부술 수 있는 벽의 개수
+    static int[][][] visited;
+    static int[] dx = {-1,1,0,0};
+    static int[] dy = {0,0,-1,1};
 
-    static int[] dx = {0,0,-1,1};
-    static int[] dy = {1,-1,0,0};
-    
-    static int bfs(int n, int m) {
-    	Queue<int[]> q = new LinkedList<>();
-    	
-        q.add(new int[] {0,0,0});
-        visited[0][0][0] = 1;
-
-
-
-        while(!q.isEmpty()){
-            int[] cur = q.poll();
-            for(int i=0;i<4;i++){
-                int nx = cur[0] + dx[i];
-                int ny = cur[1] + dy[i];
-                int canBreak = cur[2];
-
-                if(nx <0 || ny <0|| nx >=n || ny >=m)continue;
-                
-                
-                if(data[nx][ny] == 0){//길 발견
-                    if(visited[nx][ny][canBreak]==0)  {//아직 방문 x
-                        q.offer(new int[] {nx,ny,canBreak});
-                        visited[nx][ny][canBreak] = visited[cur[0]][cur[1]][canBreak] + 1;
-                        if(nx == n-1 && ny == m-1){
-                            
-                            return visited[nx][ny][canBreak];
-                        }
-                    }
-                }else{//벽 발견
-                    if(canBreak == 0){//아직 break X
-                        if(visited[nx][ny][1]==0){//부순 후 해당 위치 방문 x
-                            q.offer(new int[] {nx,ny,1});
-                            visited[nx][ny][1] = visited[cur[0]][cur[1]][0] + 1;
-                            if(nx == n-1 && ny == m-1){
-                                
-                                return visited[nx][ny][1];
-                            }
-                        }
-                    }
-                }
-
-            }
-            
-        }
-        return -1;
-        
-    }
-
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
         data = new int[n][m];
         visited = new int[n][m][2];
 
         for(int i=0;i<n;i++){
             String line = br.readLine();
             for(int j=0;j<m;j++){
-
                 data[i][j] = line.charAt(j)-'0';
             }
         }
 
+        Deque<int[]> dq = new ArrayDeque<>();
+        dq.offer(new int[]{0,0,0});
+        visited[0][0][0] = 1;
+        while(!dq.isEmpty()){
+            int[] cur = dq.poll();
+            int x = cur[0],y = cur[1],z = cur[2];
+            for(int i=0;i<4;i++){
+                int nx = x+dx[i];
+                int ny = y+dy[i];
+                if(nx<0 || ny<0 || nx >=n || ny>=m) continue;
+                if(visited[nx][ny][z] != 0) continue;
 
-        if (n == 1 && m == 1) {
-			System.out.println(1);
-			return;
-		}
+                if(data[nx][ny] == 0){
+                    visited[nx][ny][z] = visited[x][y][z] + 1;
+                    dq.offer(new int[]{nx,ny,z});
+                }else{
+                    if(z==0){
+                        visited[nx][ny][z+1] = visited[x][y][z]+1;
+                        dq.offer(new int[]{nx,ny,z+1});
+
+                    }
+                }
 
 
-        System.out.println(bfs(n,m));
+            }
+        }
+
+        int r1 = visited[n-1][m-1][0];
+        int r2 = visited[n-1][m-1][1];
+
+        if(r1==0 && r2==0) System.out.println(-1);
+        else if(r1==0 && r2!=0) System.out.println(r2);
+        else if(r2==0 && r1!=0) System.out.println(r1);
+        else System.out.println(Math.min(r1,r2));
+
     }
 }
