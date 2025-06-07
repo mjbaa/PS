c931bc7ab3ce568ba42707aa78c60748fef9721a
mjbaa <wnsqja7545@gmail.com>
2025-06-07 17:39:03 +0900

    [Gold I] Title: 벽 부수고 이동하기 3, Time: 1832 ms, Memory: 403792 KB -BaekjoonHub

--- 백준/Gold/16933. 벽 부수고 이동하기 3/벽 부수고 이동하기 3.java
+++ 백준/Gold/16933. 벽 부수고 이동하기 3/벽 부수고 이동하기 3.java
@@ -1,83 +1,76 @@
 import java.util.*;
 import java.io.*;
 
-
 public class Main {
+    static int n,m,k;
     static int[][] data;
-    static boolean[][][] visited;
+    static int[][][] dist;
     static int[] dx = {0,0,1,-1};
     static int[] dy = {1,-1,0,0};
 
-    static int bfs(int n, int m, int k) {
-        Queue<int[]> q = new ArrayDeque<>();
+    static void bfs(){
+        dist[0][0][0] = 1;
+        Deque<int[]> dq = new ArrayDeque<>();
+        dq.offer(new int[] {0,0,0,1}); // 좌표, 차원, 시간
 
-        q.add(new int[] {0,0,0,0,1});
-        visited[0][0][0] = true;
+        while(!dq.isEmpty()){
+            int[] cur = dq.poll();
+            int x = cur[0],y = cur[1], z = cur[2], time = cur[3];
 
-        while(!q.isEmpty()) {
-            int[] cur = q.poll();
-            int x = cur[0];
-            int y = cur[1];
-            int w = cur[2];
-            int d = cur[3];
-            int dist = cur[4];
+            for(int i=0;i<4;i++){
+                int nx = x+dx[i];
+                int ny = y+dy[i];
 
-            if(x==n-1 && y==m-1) {
-                return dist;
-            }
+                if(nx<0 || ny <0 || nx >= n || ny >= m) continue;
+                if(data[nx][ny]==0){
+                    if(dist[nx][ny][z]!=0) continue;
 
-            for(int i=0;i<4;i++) {
-                int nx = x + dx[i];
-                int ny = y + dy[i];
-                int nd = (d + 1) %2;
+                    dist[nx][ny][z] = time+1;
+                    dq.offer(new int[] {nx,ny,z, time+1});
+                }else{// 벽
+                    if(z < k){
+                        if(dist[nx][ny][z+1]!=0) continue;
 
-                if(nx<0 || ny < 0 || nx >=n|| ny >=m) continue;
-                
-                if(data[nx][ny]==0 && !visited[nx][ny][w]) {//길, 방문x
-                	q.add(new int[] {nx,ny,w,nd,dist+1});
-                    visited[nx][ny][w] = true;
-                    continue;
+                        if(time % 2 == 1){ // 홀수 : 낮
+                            dist[nx][ny][z+1] = time + 1;
+                            dq.offer(new int[] {nx,ny,z+1, time+1});
+                        }else{ // 밤 : 이동 안하고 하루 대기
+                            if (dist[x][y][z] < time + 1) {// 하루만 대기할 수 있도록 방문처리 해줘야함
+                                dist[x][y][z] = time + 1;
+                                dq.offer(new int[] {x, y, z, time + 1});
+                            }
+                        }
+                    }
                 }
-                
-                if(data[nx][ny]==1 && w<k && !visited[nx][ny][w+1]) {//벽, 부술 수 있음, 방문x
-                	if(d==0) {//낮
-                		q.add(new int[] {nx,ny,w+1,nd,dist+1});
-                        visited[nx][ny][w+1] =true;
-                	}else {//밤
-                		q.add(new int[] {x,y,w,nd,dist+1});
-                	}
-                }
-                
             }
         }
-        return -1;
     }
-
-
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-        int k = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
 
         data = new int[n][m];
-        visited = new boolean[n][m][k+1];
-
-        for(int i=0;i<n;i++) {
+        dist = new int[n][m][k+1];
+        for (int i = 0; i < n; i++) {
             String line = br.readLine();
-            for(int j=0;j<m;j++) {
-                data[i][j]= line.charAt(j) - '0';
+            for(int j=0;j<m;j++){
+                data[i][j] = line.charAt(j)-'0';
             }
         }
 
-        if(n==1 && m == 1) {
-            System.out.println(1);
-            return;
+        bfs();
+
+        int min = Integer.MAX_VALUE;
+        for(int i=0;i<=k;i++){
+            int val = dist[n-1][m-1][i];
+            if(val == 0) continue;
+            min = Math.min(min,val);
         }
 
-
-        System.out.println(bfs(n,m,k));
+        if(min == Integer.MAX_VALUE) System.out.println(-1);
+        else System.out.println(min);
     }
 }
