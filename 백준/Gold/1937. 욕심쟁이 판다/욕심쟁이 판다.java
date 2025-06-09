7a3da288833699ffa6be0950bc5acaa687826430
mjbaa <wnsqja7545@gmail.com>
2025-06-09 17:10:20 +0900

    [Gold III] Title: 욕심쟁이 판다, Time: 1064 ms, Memory: 64908 KB -BaekjoonHub

---
+++ 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.java
@@ -0,0 +1,65 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int n;
+    static int[][] data;
+    static long[][] dp;
+    static boolean[][] visited;
+    static int[] dx = {0,0,1,-1};
+    static int[] dy = {1,-1,0,0};
+
+    static long totalmax = Long.MIN_VALUE;
+
+    static long dfs(int sx,int sy){
+        if(dp[sx][sy] != -1){
+            return dp[sx][sy];
+        }
+
+        long curmax = 1;
+
+        for(int i=0;i<4;i++){
+            int nx = sx + dx[i];
+            int ny = sy + dy[i];
+            if(nx<0 || ny < 0 || nx >= n || ny >= n) continue;
+            if(data[nx][ny] <= data[sx][sy]) continue;
+            if(visited[nx][ny]) continue;
+
+            curmax = Math.max(curmax, dfs(nx,ny) + 1);
+        }
+        
+
+        return dp[sx][sy] = curmax;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        n = Integer.parseInt(br.readLine());
+        visited = new boolean[n][n];
+        data = new int[n][n];
+        dp = new long[n][n];
+        for(long[] row : dp) Arrays.fill(row, -1);
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++) {
+                data[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                if(dp[i][j] == -1) {
+                    visited[i][j] = true;
+                    long result = dfs(i,j);
+                    visited[i][j] = false;
+
+                    totalmax = Math.max(totalmax, result);
+                }
+            }
+        }
+
+        System.out.println(totalmax);
+    }
+}
