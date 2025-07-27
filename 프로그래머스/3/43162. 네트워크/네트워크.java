ad47f78ec6c6bfaced2bd975bf41eba420358cdc
mjbaa <wnsqja7545@gmail.com>
2025-07-27 13:03:17 +0900

    [level 3] Title: 네트워크, Time: 0.18 ms, Memory: 85.2 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,28 +1,32 @@
-import java.util.ArrayList;
-
 class Solution {
+    int n;
     boolean[] visited;
-    int answer = 0;
+    int[][] graph;
     
-    public void dfs(int n, int start, int[][] computers) {
-        visited[start] = true;
-        for(int i = 0; i < n; i++) {
-            if (i == start) continue;
-            if (!visited[i] && computers[start][i] == 1) {
-                dfs(n, i, computers);
+    void dfs(int start){
+        
+        for(int i=0;i<n;i++){
+            if(graph[start][i] == 1 && !visited[i]){
+                visited[i] = true;
+                dfs(i);
             }
         }
     }
-    
-    public int solution(int n, int[][] computers) {
+    public int solution(int N, int[][] computers) {
+        n = N;
         visited = new boolean[n];
-        for(int i = 0; i < n; i++) {
-            if (!visited[i]) {
-                dfs(n, i, computers);
-                answer++;
+        graph = computers;
+        
+        int cnt = 0;
+        for(int i=0;i<n;i++){
+            if(!visited[i]) {
+                visited[i] = true;
+                cnt++;
+                dfs(i);
             }
         }
         
-        return answer;
+        
+        return cnt;
     }
 }
