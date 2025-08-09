6a467482bb61b9bf9852f5996a27b6344ed25dca
mjbaa <wnsqja7545@gmail.com>
2025-08-09 17:26:56 +0900

    [Gold V] Title: A와 B 2, Time: 104 ms, Memory: 14288 KB -BaekjoonHub

--- 백준/Gold/12919. A와 B 2/A와 B 2.java
+++ 백준/Gold/12919. A와 B 2/A와 B 2.java
@@ -2,48 +2,42 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static StringBuilder s;
-    static StringBuilder t;
-    static int result;
+    static String s,t;
 
+    static boolean flag;
 
-    static void dfs(StringBuilder cur){
-        if(cur.length() == s.length()){
-            if(cur.toString().equals(s.toString())){
-                result = 1;
+    static void dfs(){
+        if(s.length() == t.length()){
+            if(s.equals(t)){
+                flag = true;
             }
             return;
         }
 
-        if(cur.charAt(cur.length()-1) == 'A'){
-            cur.deleteCharAt(cur.length()-1);
-            dfs(cur);
-            cur.append("A");
+        if(t.charAt(t.length()-1) == 'A'){
+            t = t.substring(0,t.length()-1);
+            dfs();
+            t = t+'A';
         }
 
-        if(cur.charAt(0) == 'B'){
-            cur.reverse();
-            cur.deleteCharAt(cur.length()-1);
-            dfs(cur);
-            cur.append("B");
-            cur.reverse();
+        if(t.charAt(0) == 'B'){
+            t = t.substring(1,t.length());
+            StringBuilder sb = new StringBuilder(t);
+            t = sb.reverse().toString();
+            dfs();
+            t = t+'B';
+            sb = new StringBuilder(t);
+            t = sb.reverse().toString();
         }
-
     }
-    public static void main(String[] args) throws Exception {
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String S = br.readLine();
-        String T = br.readLine();
+        s = br.readLine();
+        t = br.readLine();
 
-        s = new StringBuilder(S);
-        t = new StringBuilder(T);
-
-
-        dfs(t);
-
-
-        System.out.println(result);
+        dfs();
+        if(flag) System.out.println(1);
+        else System.out.println(0);
     }
-
-
 }
