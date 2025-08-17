2181c2f370c50bce3c4086f1fcc7bbf15df6a710
mjbaa <wnsqja7545@gmail.com>
2025-08-17 17:33:41 +0900

    [Gold V] Title: 집합의 표현, Time: 388 ms, Memory: 50576 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.java
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.java
@@ -2,9 +2,10 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    static int n,m;
     static int[] parent;
 
-    static int find(int x){
+    static int find(int x) {
         if(parent[x] == x) return x;
 
         return parent[x] = find(parent[x]);
@@ -16,22 +17,20 @@ public class Main {
 
         if(aRoot == bRoot) return false;
 
-        else{
-            parent[bRoot] = aRoot;
-            return true;
-        }
+        parent[bRoot] = aRoot;
+        return true;
     }
 
-    static boolean isConnected(int a, int b){
+    static boolean isSame(int a, int b){
         return find(a) == find(b);
     }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-
+        StringBuilder sb = new StringBuilder();
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
         parent = new int[n+1];
         for(int i=0;i<=n;i++){
             parent[i] = i;
@@ -39,17 +38,23 @@ public class Main {
 
         for(int i=0;i<m;i++){
             st = new StringTokenizer(br.readLine());
-            int cmd = Integer.parseInt(st.nextToken());
+            int flag = Integer.parseInt(st.nextToken());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
 
-            if(cmd == 0){
+            if(flag == 0){
                 union(a,b);
             }else{
-                if(isConnected(a,b)) System.out.println("YES");
-                else System.out.println("NO");
+                if(isSame(a,b)){
+                    sb.append("YES\n");
+                }else{
+                    sb.append("NO\n");
+                }
             }
+
+
         }
+        System.out.println(sb);
 
     }
 }
