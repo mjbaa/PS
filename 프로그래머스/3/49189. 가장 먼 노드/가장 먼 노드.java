19def65bdbc16540e47e1b384e3e3e30abf47ecb
mjbaa <wnsqja7545@gmail.com>
2025-10-28 18:21:18 +0900

    [level 3] Title: 가장 먼 노드, Time: 34.33 ms, Memory: 117 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -2,48 +2,51 @@ import java.util.*;
 
 class Solution {
     int n;
+    List<Integer>[] graph;
     int[] dist;
     boolean[] visited;
-    List<Integer>[] graph;
+    int max = 0;
+    
+    void bfs(int sidx){
+        Deque<int[]>dq = new ArrayDeque<>();
+        dq.offer(new int[] {1,0});
+        visited[1] = true;
+        
+        while(!dq.isEmpty()){
+            int[] cur = dq.poll();
+            
+            for(int next : graph[cur[0]]){
+                if(visited[next]) continue;
+                visited[next] = true;
+                dist[next] = cur[1] + 1;
+                dq.offer(new int[] {next, dist[next]});
+                
+                max = Math.max(dist[next], max);
+            }
+        }
+    }
     public int solution(int N, int[][] edge) {
         n = N;
-        dist = new int[n+1];
-        visited = new boolean[n+1];
-        graph = new List[n+1];
+        graph = new List[N+1];
         for(int i=1;i<=n;i++){
             graph[i] = new ArrayList<>();
         }
+        
+        dist = new int[n+1];
+        visited = new boolean[n+1];
         
         for(int[] e : edge){
             graph[e[0]].add(e[1]);
             graph[e[1]].add(e[0]);
         }
         
-        Deque<int[]> dq = new ArrayDeque<>();
-        dq.offer(new int[] {1,1});
-        visited[1]= true;
+        bfs(1);
         
-        int max = 0;
-        while(!dq.isEmpty()){
-            int[] cur = dq.poll();
-            int cd = cur[1];
-
-            for(int next : graph[cur[0]]){
-                if(visited[next]) continue;
-                visited[next] = true;
-                
-                int nd = cd + 1;
-                dq.offer(new int[] {next, nd});
-                max = Math.max(max,nd);
-                dist[next] = nd;
-            }
-        }
-        
-        
-        int answer = 0;
+        int cnt = 0;
         for(int d : dist){
-            if (d == max) answer++;
+            if(d == max) cnt++;
         }
-        return answer;
+        
+        return cnt;
     }
 }
