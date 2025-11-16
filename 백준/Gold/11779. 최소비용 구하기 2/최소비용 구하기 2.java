ae8e474005a152a9759ae05285b58c3f0e6fe6bd
mjbaa <wnsqja7545@gmail.com>
2025-11-16 10:06:12 +0900

    [Gold III] Title: 최소비용 구하기 2, Time: 472 ms, Memory: 51944 KB -BaekjoonHub

--- 백준/Gold/11779. 최소비용 구하기 2/최소비용 구하기 2.java
+++ 백준/Gold/11779. 최소비용 구하기 2/최소비용 구하기 2.java
@@ -1,100 +1,89 @@
 import java.util.*;
 import java.io.*;
 
-public class Main {
-	static class Edge implements Comparable<Edge>{
-		int dest;
-		int weight;
-		Edge(int dest, int weight){
-			this.dest = dest;
-			this.weight = weight;
-		}
-		
-		public int compareTo(Edge e) {
-			return Integer.compare(this.weight,  e.weight);
-		}
-	}
-	static int n;
-	static List<Edge>[] graph;
+class Main {
+    static class Node implements Comparable<Node> {
+        int no;
+        int weight;
+
+        Node(int no, int weight){
+            this.no = no;
+            this.weight = weight;
+        }
+
+        public int compareTo(Node o) {
+            return Integer.compare(this.weight, o.weight);
+        }
+    }
+
+    static int n,m;
+    static List<Node>[] graph;
     static int[] dist;
     static int[] parent;
-    
-    static void dij(int sidx) {
-    	Arrays.fill(dist, Integer.MAX_VALUE);
-    	PriorityQueue<Edge> pq = new PriorityQueue<>();
-    	pq.offer(new Edge(sidx,0));
-    	
-    	dist[sidx] = 0;
-    	parent[sidx] = -1;
-    	
-    	while(!pq.isEmpty()) {
-    		Edge cur = pq.poll();
-    		int curNode = cur.dest;
-    		int curWeight = cur.weight;
-    		
-    		if(curWeight > dist[curNode]) continue;
-    		
-    		for(Edge next : graph[curNode]) {
-    			int nextNode = next.dest;
-    			int newDist = dist[curNode] + next.weight;
-    			
-    			if(newDist < dist[nextNode]) {
-    				dist[nextNode] = newDist;
-    				parent[nextNode] = curNode;
-    				pq.offer(new Edge(nextNode, newDist));
-    			}
-    		}
-    	}
+    static int start,end;
+
+    static void dijks(){
+        dist[start] = 0;
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        pq.offer(new Node(start,0));
+
+        while(!pq.isEmpty()){
+            Node cur = pq.poll();
+
+            if(cur.weight > dist[cur.no]) continue;
+            
+            for(Node next : graph[cur.no]){
+                if(dist[next.no] > dist[cur.no]+next.weight){
+                    dist[next.no] = dist[cur.no]+next.weight;
+                    pq.offer(new Node(next.no,dist[next.no]));
+                    parent[next.no] = cur.no;
+                }
+            }
+        }
     }
-	
-	public static void main(String[] args) throws IOException {
-    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    	n = Integer.parseInt(br.readLine());
-    	graph = new List[n+1];
-    	for(int i=1;i<=n;i++) {
-    		graph[i] = new ArrayList<>();
-    	}
-    	dist = new int[n+1];
-    	parent = new int[n+1];
-    	
-    	int m = Integer.parseInt(br.readLine());
-    	StringTokenizer st;
-    	for(int i=0;i<m;i++) {
-    		st = new StringTokenizer(br.readLine());
-    		int s = Integer.parseInt(st.nextToken());
-    		int d = Integer.parseInt(st.nextToken());
-    		int w = Integer.parseInt(st.nextToken());
-    		graph[s].add(new Edge(d,w));
-    	}
-    	
-    	st = new StringTokenizer(br.readLine());
-    	int sidx = Integer.parseInt(st.nextToken());
-    	int didx = Integer.parseInt(st.nextToken());
-    	dij(sidx);
-    	
-    	StringBuilder sb = new StringBuilder();
-//    	System.out.println(dist[didx]);
-    	
-    	int cnt = 0;
-    	Stack<Integer> stack = new Stack<>();
-    	
-    	int idx = didx;
-    	while(true) {
-    		if(idx == -1) {
-    			break;
-    		}
-    		cnt++;
-    		stack.push(idx);
-    		
-    		idx = parent[idx];
-    		
-    	}
-    	sb.append(dist[didx]).append("\n");
-    	sb.append(cnt).append("\n");
-    	while(!stack.isEmpty()) {
-    		sb.append(stack.pop()).append(" ");
-    	}
-    	
-    	System.out.println(sb);
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        n = Integer.parseInt(br.readLine());
+        m = Integer.parseInt(br.readLine());
+        graph = new List[n+1];
+        dist = new int[n+1];
+        parent = new int[n+1];
+        for(int i=1;i<=n;i++){
+            graph[i] = new ArrayList<>();
+            dist[i] = Integer.MAX_VALUE;
+            parent[i] = -1;
+        }
+
+        for(int i=0;i<m;i++){
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int weight = Integer.parseInt(st.nextToken());
+            graph[a].add(new Node(b,weight));
+        }
+        st = new StringTokenizer(br.readLine());
+        start = Integer.parseInt(st.nextToken());
+        end = Integer.parseInt(st.nextToken());
+
+        dijks();
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(dist[end]).append("\n");
+
+        Stack<Integer> stack = new Stack<>();
+        int cur = end;
+        while(cur != -1) {
+            stack.push(cur);
+            cur = parent[cur];
+        }
+        sb.append(stack.size()).append("\n");
+        while(!stack.isEmpty()){
+            sb.append(stack.pop()).append(" ");
+        }
+
+        System.out.println(sb);
     }
 }
+
