0d71256ca73c52b7437e2cca1ce7698a1c9265e6
mjbaa <wnsqja7545@gmail.com>
2025-12-23 11:04:32 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 석유 시추, Time: 75.01 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/README.md
+++ 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/README.md
@@ -1,10 +1,10 @@
 # [level 2] [PCCP 기출문제] 2번 / 석유 시추 - 250136 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/250136?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/250136) 
 
 ### 성능 요약
 
-메모리: 73.6 MB, 시간: 100.99 ms
+메모리: 73.4 MB, 시간: 75.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 09월 25일 19:38:12
+2025년 12월 23일 11:04:21
 
 ### 문제 설명
 
--- 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.java
+++ 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.java
@@ -1,65 +1,65 @@
-import java.io.*;
 import java.util.*;
 
 class Solution {
-    static int[][] data;
-    static boolean[][] visited;
-    static int n,m;
-    static int[] dx = {0,0,1,-1};
-    static int[] dy = {1,-1,0,0};
-    static int[] result;
+    boolean[][] visited;
+    int n,m;
+    int[] result;
+    int[] dx = new int[] {0,0,1,-1};
+    int[] dy = new int[] {1,-1,0,0};
+    int[][] data;
     
-    void bfs(int sx,int sy){
+    void bfs(int sx, int sy){
+        Deque<int[]>dq = new ArrayDeque<>();
         Set<Integer> set = new HashSet<>();
-        Deque<int[]> dq = new ArrayDeque<>();
-        
-        int cnt = 1;
-        dq.offer(new int[] {sx ,sy});
+        dq.offer(new int[] {sx,sy});
         visited[sx][sy] = true;
-        
+        set.add(sy);
+        int cnt = 1;
         while(!dq.isEmpty()){
             int[] cur = dq.poll();
-            
-            set.add(cur[1]);
-            
-            for(int f=0;f<4;f++){
-                int nx = cur[0] + dx[f];
-                int ny = cur[1] + dy[f];
-                
+            int x = cur[0];
+            int y = cur[1];
+            for(int f = 0; f<4;f++){
+                int nx = x + dx[f];
+                int ny = y + dy[f];
                 if(nx < 0 || ny < 0 || nx >= n || ny >= m) continue;
                 if(data[nx][ny] == 0) continue;
                 if(visited[nx][ny]) continue;
                 visited[nx][ny] = true;
+                
                 dq.offer(new int[] {nx,ny});
                 cnt++;
+                set.add(ny);
             }
         }
         
-        for(int col : set){
-            result[col] += cnt;
+        for(int val : set){
+            result[val] += cnt;
         }
-        
+              
     }
     public int solution(int[][] land) {
         data = land;
-        n = data.length;
-        m = data[0].length;
+        n = land.length;
+        m = land[0].length;
         visited = new boolean[n][m];
         result = new int[m];
         
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(data[i][j] == 0) continue;
-                if(visited[i][j]) continue;
-                
-                bfs(i,j);
+                if(land[i][j] == 1 && !visited[i][j]){
+                    bfs(i,j);
+                }
             }
         }
         
-        int max = 0;
-        for(int i=0;i<m;i++){
-            max = Math.max(max, result[i]);
+        
+        
+        
+        int answer = 0;
+        for(int val : result){
+            answer = Math.max(answer,val);
         }
-        return max;
+        return answer;
     }
 }
