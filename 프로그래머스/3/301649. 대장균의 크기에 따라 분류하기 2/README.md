32f6c1d0de0535c47e204d64085ebc66088d24ce
mjbaa <wnsqja7545@gmail.com>
2026-01-06 20:37:05 +0900

    [level 3] Title: 대장균의 크기에 따라 분류하기 2, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/3/301649. 대장균의 크기에 따라 분류하기 2/README.md
+++ 프로그래머스/3/301649. 대장균의 크기에 따라 분류하기 2/README.md
@@ -1,6 +1,6 @@
 # [level 3] 대장균의 크기에 따라 분류하기 2 - 301649 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/301649?language=mysql) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/301649?language=mysql#) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 12월 31일 17:35:49
+2026년 01월 06일 20:37:03
 
 ### 문제 설명
 
--- 프로그래머스/3/301649. 대장균의 크기에 따라 분류하기 2/대장균의 크기에 따라 분류하기 2.sql
+++ 프로그래머스/3/301649. 대장균의 크기에 따라 분류하기 2/대장균의 크기에 따라 분류하기 2.sql
@@ -1,15 +1,10 @@
--- 코드를 작성해주세요
 SELECT ID, 
-CASE
-    WHEN rvalue <=0.25 then 'CRITICAL'
-    WHEN rvalue <= 0.50 then 'HIGH'
-    WHEN rvalue <= 0.75 then 'MEDIUM'
-    ELSE 'LOW'
-END as COLONY_NAME
-FROM (
-    SELECT ID, PERCENT_RANK() OVER (ORDER BY SIZE_OF_COLONY DESC) as rvalue
-    FROM ECOLI_DATA
-) as rank_table
-ORDER BY ID
-
-
+    CASE
+        when a.rvalue <= 0.25 then "CRITICAL"
+        when a.rvalue <= 0.50 then "HIGH"
+        when a.rvalue <= 0.75 then "MEDIUM"
+        else "LOW"
+    end as COLONY_NAME
+FROM (select ID, Percent_rank() over(order by size_of_colony desc) as rvalue
+from ECOLI_DATA) as a
+order by ID
