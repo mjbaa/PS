d31fd711c8b4b65457f2cc52f8bb0129f45d25b0
mjbaa <wnsqja7545@gmail.com>
2026-01-14 21:03:02 +0900

    [Gold IV] Title: 최단경로, Time: 620 ms, Memory: 107592 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -1,35 +1,55 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
     static class Node implements Comparable<Node> {
         int no, weight;
-        Node(int no, int weight) {
+        Node(int no, int weight){
             this.no = no;
             this.weight = weight;
         }
 
-        public int compareTo(Node o) {
-            return this.weight - o.weight;
+        public int compareTo(Node o){
+            return Integer.compare(this.weight, o.weight);
         }
     }
+
     static int v,e;
     static int[] dist;
     static List<Node>[] graph;
-    public static void main(String[] args) throws IOException {
+
+    static void dijks(int start){
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        pq.offer(new Node(start,0));
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[start] = 0;
+
+        while(!pq.isEmpty()){
+            Node cur = pq.poll();
+
+            if(dist[cur.no] < cur.weight) continue;
+
+            for(Node next : graph[cur.no]){
+                if(dist[next.no] > dist[cur.no] + next.weight){
+                    dist[next.no] = dist[cur.no] + next.weight;
+                    pq.offer(new Node(next.no, dist[next.no]));
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         v = Integer.parseInt(st.nextToken());
         e = Integer.parseInt(st.nextToken());
-
-        dist = new int[v+1];
-        Arrays.fill(dist, Integer.MAX_VALUE);
-        int start = Integer.parseInt(br.readLine());
-
         graph = new List[v+1];
-        for(int i = 1; i <= v; i++) {
+        for(int i=1;i<=v;i++){
             graph[i] = new ArrayList<>();
         }
+        dist = new int[v+1];
+
+        int start = Integer.parseInt(br.readLine());
 
         for(int i=0;i<e;i++){
             st = new StringTokenizer(br.readLine());
@@ -37,30 +57,14 @@ public class Main {
             int b = Integer.parseInt(st.nextToken());
             int weight = Integer.parseInt(st.nextToken());
             graph[a].add(new Node(b,weight));
-//            graph[b].add(new Node(a,weight));
         }
 
-        dist[start] = 0;
-        PriorityQueue<Node> pq = new PriorityQueue<>();
-        pq.offer(new Node(start,0));
-        while(!pq.isEmpty()){
-            Node cur = pq.poll();
-
-            for(Node next : graph[cur.no]){
-                if(dist[next.no] > dist[cur.no] + next.weight){
-                    dist[next.no] = dist[cur.no] + next.weight;
-                    pq.offer(new Node(next.no,dist[next.no]));
-                }
-            }
-        }
-
+        dijks(start);
         StringBuilder sb = new StringBuilder();
-        for(int i = 1; i <= v; i++) {
-            if(dist[i] == Integer.MAX_VALUE){
-                sb.append("INF");
-            }else{
-                sb.append(dist[i]);
-            }
+        for(int i=1;i<=v;i++){
+            int val = dist[i];
+            if(val == Integer.MAX_VALUE) sb.append("INF");
+            else sb.append(val);
             sb.append("\n");
         }
 
