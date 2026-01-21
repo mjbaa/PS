7d306d668adf8b7b7cbde34d08aa37121987277e
mjbaa <wnsqja7545@gmail.com>
2026-01-21 14:22:38 +0900

    [Gold II] Title: 보석 도둑, Time: 1316 ms, Memory: 117260 KB -BaekjoonHub

--- 백준/Gold/1202. 보석 도둑/보석 도둑.java
+++ 백준/Gold/1202. 보석 도둑/보석 도둑.java
@@ -2,60 +2,62 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	static class Item implements Comparable<Item>{
-		int weight;
-		int value;
-		Item(int weight,int value){
-			this.weight = weight;
-			this.value = value;
-			
-		}
-		
+    static class Item implements Comparable<Item>{
+        int weight, value;
+        Item(int weight, int value){
+            this.weight = weight;
+            this.value = value;
+        }
 
-		public int compareTo(Item o) {
-		    return Integer.compare(o.value, this.value); // value 내림차순
-		}
+        public int compareTo(Item o) {
+            return Integer.compare(this.weight, o.weight);
+        }
+    }
 
-	}
-
-    public static void main(String[] args) throws IOException {
+    static int n,k;
+    static List<Item> items;
+    static List<Integer> bags;
+    static boolean[] used;
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int k = Integer.parseInt(st.nextToken());
-        
-        Item[] items = new Item[n];
-        for(int i=0;i<n;i++) {
-        	st = new StringTokenizer(br.readLine());
-        	items[i] = new Item(Integer.parseInt(st.nextToken()),Integer.parseInt(st.nextToken()) );
+        n = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
+
+        items = new ArrayList<>(n);
+        for(int i=0;i<n;i++){
+            st = new StringTokenizer(br.readLine());
+            int weight = Integer.parseInt(st.nextToken());
+            int value = Integer.parseInt(st.nextToken());
+            items.add(new Item(weight,value));
         }
-        
-        int[] bags = new int[k];
-        for(int i=0;i<k;i++) {
-        	bags[i] = Integer.parseInt(br.readLine());
+        Collections.sort(items); // 무게 기반 오름차순
+
+        bags = new ArrayList<>(n);
+        for(int i=0;i<k;i++){
+            bags.add(Integer.parseInt(br.readLine()));
         }
-        
-        Arrays.sort(items,(a,b) -> a.weight - b.weight); // 무게 순 오름차순 정렬
-        Arrays.sort(bags); // 가방 저장 공간 순 오름차순 정렬
-        
-        PriorityQueue<Item> pq = new PriorityQueue<>();
-        //pq : 담을 수 있는 item들 가치 기준 내림차순 정렬
-        
-        int itemIdx = 0;
+        Collections.sort(bags); // 용량 기반 오름차순
+
+        PriorityQueue<Item> pq = new PriorityQueue<>((a,b) -> b.value - a.value); // 가치 기반 내림차순
         long sum = 0;
-        for(int i=0;i<k;i++) {
-        	int capacity = bags[i];
-        	while(itemIdx < n && items[itemIdx].weight <= capacity ) {
-        		pq.offer(items[itemIdx]);
-        		itemIdx++;
-        	}
-        	
-        	if (!pq.isEmpty()) {
-        	    sum += pq.poll().value;
-        	}
+        int idx = 0;
+        for(int i=0;i<k;i++){
+            int size = bags.get(i);
+
+            while(idx < n && items.get(idx).weight <= size){
+                pq.add(items.get(idx));
+                idx++;
+            }
+
+            if(!pq.isEmpty()){
+                sum += pq.poll().value;
+            }
         }
-        
+
         System.out.println(sum);
-        
+
     }
+
+
 }
