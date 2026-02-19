0deb2e1a30e82803fde405966607a7b716da1daa
mjbaa <wnsqja7545@gmail.com>
2026-02-19 16:43:11 +0900

    [Gold II] Title: 친구 네트워크, Time: 824 ms, Memory: 92932 KB -BaekjoonHub

---
+++ 백준/Gold/4195. 친구 네트워크/친구 네트워크.java
@@ -0,0 +1,74 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int f;
+//    static Map<String,List<String>> graph;
+    static Map<String,String> parent;
+    static Map<String, Integer> size;
+
+    static String find(String a){
+        if(parent.get(a).equals(a)) return a;
+        
+        String root = find(parent.get(a));
+        parent.put(a, root);
+        return root;
+    }
+
+    static void union(String a, String b){
+        String aRoot = find(a);
+        String bRoot = find(b);
+
+        if(aRoot.equals(bRoot)) return;
+
+        parent.put(aRoot, bRoot);
+        size.put(bRoot, size.get(aRoot) + size.get(bRoot));
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        int t = Integer.parseInt(br.readLine());
+        for(int tc = 1; tc <= t; tc++){
+//            graph = new HashMap<>();
+            parent = new HashMap<>();
+            size = new HashMap<>();
+
+            f = Integer.parseInt(br.readLine());
+            for(int i=0; i<f; i++){
+                st = new StringTokenizer(br.readLine());
+                String a = st.nextToken();
+                String b = st.nextToken();
+
+//                if(!graph.containsKey(a)){
+//                    graph.put(a,new ArrayList<>());
+//                }
+//                graph.get(a).add(b);
+                if(!parent.containsKey(a)){
+                    parent.put(a,a);
+                }
+                if(!size.containsKey(a)){
+                    size.put(a,1);
+                }
+
+//                if(!graph.containsKey(b)){
+//                    graph.put(b,new ArrayList<>());
+//                }
+//                graph.get(b).add(a);
+                if(!parent.containsKey(b)){
+                    parent.put(b,b);
+                }
+                if(!size.containsKey(b)){
+                    size.put(b,1);
+                }
+
+                union(a,b);
+                sb.append(size.get(find(a))).append('\n');
+
+            }
+
+        }
+        System.out.println(sb);
+    }
+}
