1285025d78ed7132aacdcb3a8cb6e26ec32af306
mjbaa <wnsqja7545@gmail.com>
2026-03-23 21:23:51 +0900

    [Gold V] Title: 최소비용 구하기, Time: 496 ms, Memory: 51936 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
@@ -1,74 +1,74 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
-	static class Edge implements Comparable<Edge>{
-		
-		int dest;
-		int weight;
-		Edge(int dest, int weight){
-			this.dest = dest;
-			this.weight = weight;
-		}
-		
-		public int compareTo(Edge e) {
-			return Integer.compare(this.weight,  e.weight);
-		}
-	}
-	
-    static int n;
-    static List<Edge>[] graph;
-    static int[] dist;
-    	
-    static void dij(int start) {
-    	Arrays.fill(dist, Integer.MAX_VALUE);
-    	dist[start] = 0;
-    	PriorityQueue<Edge> pq = new PriorityQueue<>();
-    	pq.offer(new Edge(start,0));
-    	
-    	while(!pq.isEmpty()) {
-    		Edge cur = pq.poll();
-    		int curNode = cur.dest;
-    		int curWeight = cur.weight; 
-    		
-    		if(curWeight > dist[curNode]) continue;
-    		
-    		for(Edge next : graph[curNode]) {
-    			int nextNode = next.dest;
-    			int newDist = curWeight + next.weight;
-    			
-    			if(newDist < dist[nextNode]) {
-    				dist[nextNode] = newDist;
-    				pq.offer(new Edge(nextNode, newDist));
-    			}
-    		}
-    	}
-    	
-    }
-    public static void main(String[] args) throws IOException {
-    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    	n = Integer.parseInt(br.readLine());
-    	graph = new List[n+1];
-    	dist = new int[n+1];
-    	for(int i=1;i<=n;i++) {
-    		graph[i] = new LinkedList<>();
-    	}
-    	
-    	int m = Integer.parseInt(br.readLine());
-        StringTokenizer st;
-    	for(int i=0;i<m;i++) {
-        	st = new StringTokenizer(br.readLine());
-        	int s = Integer.parseInt(st.nextToken());
-        	int d = Integer.parseInt(st.nextToken());
-        	int w = Integer.parseInt(st.nextToken());
-        	
-        	graph[s].add(new Edge(d,w));
+    static class Node implements Comparable<Node> {
+        int no,weight;
+        Node(int no, int weight){
+            this.no = no;
+            this.weight = weight;
         }
-    	st = new StringTokenizer(br.readLine());
-    	int sidx = Integer.parseInt(st.nextToken());
-    	int didx = Integer.parseInt(st.nextToken());
-    	dij(sidx);
-        
-        System.out.println(dist[didx]);
+
+        @Override
+        public int compareTo(Node o) {
+            return Integer.compare(this.weight, o.weight);
+        }
+
     }
+    static int n,m;
+    static int[] dist;
+    static List<Node>[] graph;
+
+    static void dijks(int start){
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        pq.offer(new Node(start,0));
+        dist[start] = 0;
+
+        while(!pq.isEmpty()){
+            Node cur = pq.poll();
+            
+            if(cur.weight > dist[cur.no]) continue;
+
+            for(Node next : graph[cur.no]){
+                if(dist[next.no] > dist[cur.no] + next.weight){
+                    dist[next.no] = dist[cur.no] + next.weight;
+                    pq.offer(new Node(next.no, dist[next.no]));
+                }
+            }
+        }
+    }
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        n = Integer.parseInt(br.readLine());
+        m = Integer.parseInt(br.readLine());
+
+        dist = new int[n+1];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+
+
+        graph = new List[n+1];
+        for(int i=1;i<=n;i++){
+            graph[i] = new ArrayList<>();
+        }
+
+        for(int i=0;i<m;i++){
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int weight = Integer.parseInt(st.nextToken());
+
+            graph[a].add(new Node(b,weight));
+        }
+
+        st = new StringTokenizer(br.readLine());
+        int start = Integer.parseInt(st.nextToken());
+        int end = Integer.parseInt(st.nextToken());
+
+        dijks(start);
+
+        System.out.println(dist[end]);
+    }
+
 }
