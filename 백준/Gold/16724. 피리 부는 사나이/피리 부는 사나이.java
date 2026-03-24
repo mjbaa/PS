be66358a16501f63985c385ad1fe89d97c89ad88
mjbaa <wnsqja7545@gmail.com>
2026-03-24 21:30:59 +0900

    [Gold III] Title: 피리 부는 사나이, Time: 456 ms, Memory: 65028 KB -BaekjoonHub

--- 백준/Gold/16724. 피리 부는 사나이/피리 부는 사나이.java
+++ 백준/Gold/16724. 피리 부는 사나이/피리 부는 사나이.java
@@ -2,67 +2,72 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int[] parent;
     static int n, m;
     static char[][] data;
-    
-    static int find(int x) {
-        if (parent[x] == x) return x;
-        return parent[x] = find(parent[x]);
-    }
-    
-    static void union(int x, int y) {
-        int rootX = find(x);
-        int rootY = find(y);
-        if (rootX != rootY) {
-            parent[rootY] = rootX;
-        }
-    }
-    
-    static int getNext(int idx) {
-        int x = idx / m;
-        int y = idx % m;
-        char dir = data[x][y];
-        
-        switch(dir) {
-            case 'U': return (x-1) * m + y;
-            case 'D': return (x+1) * m + y;
-            case 'L': return x * m + (y-1);
-            case 'R': return x * m + (y+1);
-        }
-        return -1;
+    static int[] parent;
+
+    static int[] dx = {0, 0, 1, -1};   // R, L, D, U
+    static int[] dy = {1, -1, 0, 0};
+
+    static int getDir(char c) {
+        if (c == 'R') return 0;
+        else if (c == 'L') return 1;
+        else if (c == 'D') return 2;
+        else return 3;
     }
 
-    public static void main(String[] args) throws IOException {
+    static int find(int a) {
+        if (parent[a] == a) return a;
+        return parent[a] = find(parent[a]);
+    }
+
+    static void union(int a, int b) {
+        int aRoot = find(a);
+        int bRoot = find(b);
+
+        if (aRoot == bRoot) return;
+        parent[bRoot] = aRoot;
+    }
+
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
-        
-        parent = new int[n * m];
+
         data = new char[n][m];
-        
+        parent = new int[n * m];
+
         for (int i = 0; i < n * m; i++) {
             parent[i] = i;
         }
-        
+
         for (int i = 0; i < n; i++) {
             String line = br.readLine();
             for (int j = 0; j < m; j++) {
                 data[i][j] = line.charAt(j);
             }
         }
-        
-        for (int i = 0; i < n * m; i++) {
-            int next = getNext(i);
-            union(i, next);
+
+        for (int x = 0; x < n; x++) {
+            for (int y = 0; y < m; y++) {
+                int dir = getDir(data[x][y]);
+                int nx = x + dx[dir];
+                int ny = y + dy[dir];
+
+                int curIdx = x * m + y;
+                int nextIdx = nx * m + ny;
+
+                union(curIdx, nextIdx);
+            }
         }
-        
-        Set<Integer> roots = new HashSet<>();
+
+        Set<Integer> set = new HashSet<>();
         for (int i = 0; i < n * m; i++) {
-            roots.add(find(i));
+            set.add(find(i));
         }
-        
-        System.out.println(roots.size());
+
+        System.out.println(set.size());
     }
 }
