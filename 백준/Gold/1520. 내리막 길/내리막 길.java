0dbf38ca26782f1b9af7c8ee03220e6712d2987f
mjbaa <wnsqja7545@gmail.com>
2026-04-05 00:54:01 +0900

    [Gold III] Title: 내리막 길, Time: 356 ms, Memory: 36216 KB -BaekjoonHub

--- 백준/Gold/1520. 내리막 길/내리막 길.java
+++ 백준/Gold/1520. 내리막 길/내리막 길.java
@@ -5,51 +5,48 @@ public class Main {
     static int n,m;
     static int[][] data;
     static int[][] dp;
-
-    static int[] dx = {0,0,1,-1};
+    static int[] dx={0,0,1,-1};
     static int[] dy = {1,-1,0,0};
 
-    static int dfs(int sx, int sy) {
+    static int dfs(int sx, int sy){
         if(dp[sx][sy] != -1) return dp[sx][sy];
 
         int sum = 0;
-
-        for(int i=0;i<4;i++){
-            int nx = sx + dx[i];
-            int ny = sy + dy[i];
-            if(nx<0 || ny <0 || nx >= n || ny >= m) continue;
-            if(data[nx][ny] >= data[sx][sy]) continue;
-
-            sum += dfs(nx, ny);
+        for(int f=0;f<4;f++){
+            int nx = sx + dx[f];
+            int ny = sy + dy[f];
+            if(nx < 0 || ny < 0 || nx >= n || ny >= m) continue;
+            if(data[sx][sy] <= data[nx][ny]) continue;
+            sum += dfs(nx,ny);
         }
 
         return dp[sx][sy] = sum;
+
     }
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
 
-        dp = new int[n][m];
-        for(int i = 0;i<n;i++){
-            Arrays.fill(dp[i], -1);
-        }
-        dp[n-1][m-1] = 1;
-
         data = new int[n][m];
-        for (int i = 0; i < n; i++) {
+        dp = new int[n][m];
+
+        for(int i=0;i<n;i++){
             st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < m; j++) {
+            for(int j=0;j<m;j++){
                 data[i][j] = Integer.parseInt(st.nextToken());
+                dp[i][j] = -1;
             }
         }
 
+        dp[n-1][m-1] = 1;
         int result = dfs(0,0);
 
         System.out.println(result);
 
-
     }
+
+
 }
